7a7db1fcf8c969a69d78894746564284d5e26e8e
Wolf1824 <makhijaniankush24@gmail.com>
2025-07-02 14:54:22 -0400

    docker

--- backend/src/main/java/com/ExpenseTracker/ExpenseTracker/config/SecurityConfig.java
+++ backend/src/main/java/com/ExpenseTracker/ExpenseTracker/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // replace with frontend URL
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:3000")); // replace with frontend URL
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // optional if using cookies/auth headers
